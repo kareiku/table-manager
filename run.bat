c21f4d11014f15cbf4a50717a0a11777f83554d2
kareiku <xaxvu7@gmail.com>
2024-11-16 03:00:53 +0100

    commit message unspecified

--- run.bat
+++ run.bat
@@ -1,14 +1,24 @@
 @echo off
 python --version
-if %errorlevel% neq 0 exit
+if %errorlevel% neq 0 (
+    echo Python is not installed. Press any key to exit...
+    pause
+    exit /b
+)
 
 pip --version
-if %errorlevel% neq 0 python -m ensurepip
+if %errorlevel% neq 0 (
+    python -m ensurepip
+)
 
 pip show tksheet
-if %errorlevel% neq 0 pip install tksheet
+if %errorlevel% neq 0 (
+    pip install tksheet
+)
 
 pip show pandas
-if %errorlevel% neq 0 pip install pandas
+if %errorlevel% neq 0 (
+    pip install pandas
+)
 
-python .\main.py
+python ./main.py
