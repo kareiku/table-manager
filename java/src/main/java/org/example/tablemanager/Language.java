cf72d070ca638d5904b243142da1e15f7c1e02a7
kareiku <xaxvu7@gmail.com>
2024-12-23 00:44:36 +0100

    Enhanced the Language enumerator

--- java/src/main/java/org/example/tablemanager/Language.java
+++ java/src/main/java/org/example/tablemanager/Language.java
@@ -1,16 +1,30 @@
 package org.example.tablemanager;
 
+import java.util.Map;
+
 public enum Language {
-    en_US("Excel Table Manager", "Exit", "Filter by..."),
-    es_ES("Gestor de tablas Excel", "Salir", "Filtrar por...");
+    en_US(Map.ofEntries(
+            Map.entry(Key.Title, "Excel Table Manager"),
+            Map.entry(Key.Exit, "Exit"),
+            Map.entry(Key.Filter, "Filter by..."))),
+    es_ES(Map.ofEntries(
+            Map.entry(Key.Title, "Gestor de tablas Excel"),
+            Map.entry(Key.Exit, "Salir"),
+            Map.entry(Key.Filter, "Filtrar por...")));
 
-    private final String[] messages;
+    private final Map<Key, String> messages;
 
-    Language(String... messages) {
+    Language(Map<Key, String> messages) {
         this.messages = messages;
     }
 
-    public String get(int index) {
-        return this.messages.length > index ? this.messages[index] : "";
+    public String get(Key key) {
+        return this.messages.get(key);
+    }
+
+    public enum Key {
+        Title,
+        Exit,
+        Filter
     }
 }
