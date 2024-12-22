e8722b9e907eb6433483ac418994a8d083931dca
kareiku <xaxvu7@gmail.com>
2024-12-22 22:28:40 +0100

    Empty commit

--- java/src/main/java/org/example/tablemanager/Main.java
+++ java/src/main/java/org/example/tablemanager/Main.java
@@ -1,17 +1,32 @@
 package org.example.tablemanager;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.scene.Node;
 
+import java.io.File;
 import java.io.IOException;
 
 public class Main extends Application {
+    private static final String ICON = new File("").getAbsolutePath() + "/src/main/resources/icon.ico";
+    private static final String COLOR = "#004490";
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        GridPane root = new GridPane();
+
+        Label first_column_var = new Label();
+        Label second_column_var = new Label();
+        Label third_column_var = new Label();
+
+        Scene scene = new Scene(root, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
