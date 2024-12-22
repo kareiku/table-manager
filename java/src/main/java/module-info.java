1156f8e51a21b537529cb7ebeca38ecb65c8a7a6
kareiku <xaxvu7@gmail.com>
2024-12-23 00:13:38 +0100

    Empty commit

---
+++ java/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module org.example.tablemanager {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    exports org.example.tablemanager;
+}
--- java/src/main/java/org/example/tablemanager/Main.java
+++ java/src/main/java/org/example/tablemanager/Main.java
@@ -2,37 +2,56 @@ package org.example.tablemanager;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableView;
-import javafx.scene.control.ToggleButton;
+import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-import javafx.scene.Node;
 
-import java.io.File;
-import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
-    private static final String ICON = new File("").getAbsolutePath() + "/src/main/resources/icon.ico";
+    private static final String ICON_PATH = "icon.png";
     private static final String COLOR = "#004490";
+    private static final Language LANG = Language.es_ES;
 
     @Override
-    public void start(Stage stage) throws IOException {
-        GridPane root = new GridPane();
+    public void start(Stage stage) {
+        try {
+            GridPane root = new GridPane();
 
-        Label first_column_var = new Label();
-        Label second_column_var = new Label();
-        Label third_column_var = new Label();
+            root.setMinSize(500, 300);
+            stage.setTitle(LANG.get(0));
+            stage.setMaximized(true);
+            Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream(ICON_PATH)));
+            stage.getIcons().add(icon);
 
-        Scene scene = new Scene(root, 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+            Scene scene = new Scene(root, 320, 240);
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception ex) {
+            assert false : ex.getMessage();
+        }
     }
 
     public static void main(String[] args) {
-        launch();
+        try {
+            launch();
+        } catch (Exception ex) {
+            assert false : ex.getMessage();
+        }
+    }
+
+    private enum Language {
+        en_US("Excel Table Manager", "Exit", "Filter by..."),
+        es_ES("Gestor de tablas Excel", "Salir", "Filtrar por...");
+
+        private final String[] messages;
+
+        Language(String... messages) {
+            this.messages = messages;
+        }
+
+        public String get(int index) {
+            return this.messages.length > index ? this.messages[index] : "";
+        }
     }
 }
