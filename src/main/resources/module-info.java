17597459fcbea12669d0ec854ee380a30fe1ab8f
kareiku <xaxvu7@gmail.com>
2024-12-23 17:48:53 +0100

    Empty commit

--- src/main/resources/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.tablemanager {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-    requires org.apache.commons.csv;
-    requires org.apache.poi.ooxml;
-
-    exports org.example.tablemanager;
-}
