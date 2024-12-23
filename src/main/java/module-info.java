7522304a6d0c064f736f72ce690ccf2f7bd7ec61
kareiku <xaxvu7@gmail.com>
2024-12-23 18:37:45 +0100

    fixed

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module org.example.tablemanager {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires org.apache.poi.poi;
+    requires org.apache.poi.ooxml;
+    requires org.apache.commons.csv;
+
+    exports org.example.tablemanager;
+}
