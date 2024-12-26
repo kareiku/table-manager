3237a3d6ae22778b184d70773e5595efd315fb62
kareiku <xaxvu7@gmail.com>
2024-12-26 05:01:17 +0100

    Added exporting. Tested it and it's working.

--- src/main/java/org/example/Controller.java
+++ src/main/java/org/example/Controller.java
@@ -1,11 +1,41 @@
 package org.example;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ChoiceDialog;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Controller {
+    private record Filter(int columnIndex, String value) {
+    }
+
+    private final Language language;
+    private final TableView<ObservableList<String>> tableView;
+    private final ObservableList<ObservableList<String>> data;
+    private final List<Filter> filterValues;
+
+    public Controller(Language language, TableView<ObservableList<String>> tableView) {
+        this.language = language;
+        this.tableView = tableView;
+        this.data = FXCollections.observableArrayList();
+        this.filterValues = new ArrayList<>();
+    }
+
     public List<List<String>> loadSheet(Sheet sheet) {
         List<List<String>> rows = new ArrayList<>();
         sheet.forEach(row -> {
@@ -14,5 +44,126 @@ public class Controller {
             rows.add(cells);
         });
         return rows;
+    }
+
+    public void showAlert(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(this.language.get(Language.Key.Title));
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    public File chooseOpenFile(Stage stage) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle(this.language.get(Language.Key.SelectFile));
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter(this.language.get(Language.Key.ExcelFiles), "*.xls", "*.xlsx", "*.xlsm"));
+        return fileChooser.showOpenDialog(stage);
+    }
+
+    public File chooseSaveFile(Stage stage) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle(this.language.get(Language.Key.SaveFile));
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter(this.language.get(Language.Key.ExcelFiles), "*.xlsx"));
+        return fileChooser.showSaveDialog(stage);
+    }
+
+    public Sheet chooseSheet(Workbook workbook) {
+        List<String> sheetNames = new ArrayList<>();
+        workbook.iterator().forEachRemaining(sheet -> sheetNames.add(sheet.getSheetName()));
+
+        ChoiceDialog<String> sheetDialog = new ChoiceDialog<>(sheetNames.get(0), sheetNames);
+        sheetDialog.setTitle(this.language.get(Language.Key.Title));
+        sheetDialog.setHeaderText(null);
+        sheetDialog.setContentText(this.language.get(Language.Key.SelectSheet));
+
+        Optional<String> sheetNameOptional = sheetDialog.showAndWait();
+        return sheetNameOptional.map(workbook::getSheet).orElse(null);
+    }
+
+    public void openFile(Stage stage) {
+        File file = this.chooseOpenFile(stage);
+        if (file != null) {
+            try (Workbook workbook = WorkbookFactory.create(file)) {
+                Sheet sheet = this.chooseSheet(workbook);
+                this.tableView.getItems().clear();
+                this.tableView.getColumns().clear();
+                List<List<String>> excelData = this.loadSheet(sheet);
+                excelData.forEach(excelDatum -> this.data.add(FXCollections.observableArrayList(excelDatum)));
+                this.tableView.setItems(this.data.stream().skip(1).collect(Collectors.toCollection(FXCollections::observableArrayList)));
+                for (int i = 0; i < excelData.get(0).size(); i++) {
+                    int currentColumn = i;
+                    TableColumn<ObservableList<String>, String> column = new TableColumn<>(this.data.get(0).get(i));
+                    column.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue().get(currentColumn)));
+                    this.tableView.getColumns().add(column);
+                }
+            } catch (Exception ignore) {
+                this.showAlert(this.language.get(Language.Key.OpenError));
+            }
+        }
+    }
+
+    public void exportFilteredData(Stage stage) {
+        File file = this.chooseSaveFile(stage);
+        if (file != null) {
+            try (Workbook workbook = new XSSFWorkbook()) {
+                Sheet sheet = workbook.createSheet(this.language.get(Language.Key.FilteredDataSheetName));
+                Row header = sheet.createRow(0);
+                this.tableView.getColumns().forEach(column -> header
+                        .createCell(this.tableView.getColumns().indexOf(column))
+                        .setCellValue(column.getText()));
+                for (int i = 0; i < this.tableView.getItems().size(); i++) {
+                    Row row = sheet.createRow(i + 1);
+                    ObservableList<String> item = this.tableView.getItems().get(i);
+                    this.tableView.getColumns().forEach(col -> {
+                        Object cell = col.getCellObservableValue(item).getValue();
+                        row.createCell(this.tableView.getColumns().indexOf(col))
+                                .setCellValue(cell == null ? "" : cell.toString());
+                    });
+                }
+                workbook.write(new FileOutputStream(file));
+            } catch (Exception ignore) {
+                this.showAlert(this.language.get(Language.Key.ExportError));
+            }
+        }
+    }
+
+    public void setFilterColumn(int filterIndex, String columnName) {
+        int columnIndex = this.getFilterColumnIndex(columnName);
+        Filter filter = this.filterValues.get(filterIndex);
+        String value = filter == null ? "" : filter.value;
+        if (columnIndex >= 0) {
+            this.filterValues.set(filterIndex, new Filter(columnIndex, value));
+        }
+    }
+
+    private int getFilterColumnIndex(String columnName) {
+        ObservableList<String> columns = this.data.get(0);
+        int columnIndex = -1;
+        int i = 0;
+        while (columnIndex == -1 && i < columns.size()) {
+            if (columns.get(i).equals(columnName)) {
+                columnIndex = i;
+            }
+            i++;
+        }
+        return columnIndex;
+    }
+
+    public void setFilterValue(int filterIndex, String value) {
+        this.filterValues.set(filterIndex, new Filter(-1, value));
+    }
+
+    public void filter() {
+        String value = this.filterValues.get(0).value; // placeholder fixme (and below too)
+        if (value.isEmpty()) {
+            this.tableView.setItems(this.data.stream().skip(1).collect(Collectors.toCollection(FXCollections::observableArrayList)));
+        } else {
+            this.tableView.setItems(this.data.stream().skip(1).filter(list -> {
+                return false;
+            }).filter(list -> {
+                return false;
+            }).collect(Collectors.toCollection(FXCollections::observableArrayList)));
+        }
     }
 }
--- src/main/java/org/example/TableManager.java
+++ src/main/java/org/example/TableManager.java
@@ -1,9 +1,6 @@
 package org.example;
 
 import javafx.application.Application;
-import javafx.beans.property.ReadOnlyObjectWrapper;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
@@ -11,26 +8,10 @@ import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
-
-import java.io.File;
-import java.util.*;
-import java.util.stream.Collectors;
 
 public final class TableManager extends Application {
     private static final Language LANG = Language.en_US;
-    private Controller controller;
-    private Stage stage;
-
-    private ComboBox<String> firstFilteringColumn;
-    private TextField firstFilteringField;
-    private ComboBox<String> secondFilteringColumn;
-    private TextField secondFilteringField;
-    private TableView<ObservableList<String>> tableView;
 
     public static void main(String[] args) {
         TableManager.launch(args);
@@ -38,170 +19,72 @@ public final class TableManager extends Application {
 
     @Override
     public void start(Stage stage) {
-        this.controller = new Controller();
-        this.stage = stage;
+        TableView<ObservableList<String>> tableView = new TableView<>();
+        Controller controller = new Controller(LANG, tableView);
+
         stage.setTitle(LANG.get(Language.Key.Title));
         stage.getIcons().add(new Image("file:icon.png"));
         stage.setMaximized(true);
         stage.setResizable(true);
 
-        BorderPane root = new BorderPane();
+        ComboBox<String> firstFilteringColumn = new ComboBox<>();
+        ComboBox<String> secondFilteringColumn = new ComboBox<>();
+        TextField firstFilteringField = new TextField();
+        TextField secondFilteringField = new TextField();
 
         MenuItem openItem = new MenuItem(LANG.get(Language.Key.OpenFile));
-        openItem.setOnAction(ignored -> this.openFile());
-
         MenuItem exportItem = new MenuItem(LANG.get(Language.Key.ExportFile));
-        exportItem.setOnAction(ignored -> System.console());
-
         MenuItem exitItem = new MenuItem(LANG.get(Language.Key.Exit));
+
+        openItem.setOnAction(ignored -> controller.openFile(stage));
+        exportItem.setOnAction(ignored -> controller.exportFilteredData(stage));
         exitItem.setOnAction(ignored -> System.exit(0));
+
+        BorderPane root = new BorderPane();
 
         root.setTop(new MenuBar(new Menu(LANG.get(Language.Key.FileMenu), null, openItem, exportItem, exitItem)));
 
         HBox controls = new HBox(10);
         controls.setPadding(new Insets(10));
 
-        this.firstFilteringColumn = new ComboBox<>();
-        this.firstFilteringColumn.setPromptText(LANG.get(Language.Key.ColumnSelectPlaceholder));
-        this.firstFilteringColumn.setPrefWidth(140);
+        firstFilteringColumn.setPromptText(LANG.get(Language.Key.ColumnSelectPlaceholder));
+        firstFilteringColumn.setPrefWidth(140);
+        firstFilteringColumn.getSelectionModel().selectedItemProperty().addListener((ignoreObservable, ignoreOldValue, newValue) -> {
+            if (newValue != null) {
+                controller.setFilterColumn(0, newValue);
+            }
+        });
 
-        this.firstFilteringField = new TextField();
-        this.firstFilteringField.setPromptText(LANG.get(Language.Key.FilterPlaceholder));
-        this.firstFilteringField.setPrefWidth(150);
-        this.firstFilteringField.textProperty().addListener((ignored0, ignored1, ignored2) -> System.nanoTime());
+        firstFilteringField.setPromptText(LANG.get(Language.Key.FilterPlaceholder));
+        firstFilteringField.setPrefWidth(150);
+        firstFilteringField.textProperty().addListener((ignoreObservable, ignoreOldValue, newValue) -> {
+            controller.setFilterValue(0, newValue);
+            controller.filter();
+        });
 
-        this.secondFilteringColumn = new ComboBox<>();
-        this.secondFilteringColumn.setPromptText(LANG.get(Language.Key.ColumnSelectPlaceholder));
-        this.secondFilteringColumn.setPrefWidth(140);
+        secondFilteringColumn.setPromptText(LANG.get(Language.Key.ColumnSelectPlaceholder));
+        secondFilteringColumn.setPrefWidth(140);
+        secondFilteringColumn.getSelectionModel().selectedItemProperty().addListener((ignoreObserver, ignoreOldValue, newValue) -> {
+            if (newValue != null) {
+                controller.setFilterColumn(1, newValue);
+            }
+        });
 
-        this.secondFilteringField = new TextField();
-        this.secondFilteringField.setPromptText(LANG.get(Language.Key.FilterPlaceholder));
-        this.secondFilteringField.setPrefWidth(150);
-        this.secondFilteringField.textProperty().addListener((ignored0, ignored1, ignored2) -> System.nanoTime());
+        secondFilteringField.setPromptText(LANG.get(Language.Key.FilterPlaceholder));
+        secondFilteringField.setPrefWidth(150);
+        secondFilteringField.textProperty().addListener((ignoreObservable, ignoreOldValue, newValue) -> {
+            controller.setFilterValue(1, newValue);
+            controller.filter();
+        });
 
-        controls.getChildren().addAll(this.firstFilteringColumn, this.firstFilteringField, this.secondFilteringColumn, this.secondFilteringField);
+        controls.getChildren().addAll(firstFilteringColumn, firstFilteringField, secondFilteringColumn, secondFilteringField);
         root.setCenter(controls);
 
-        this.tableView = new TableView<>();
-        this.tableView.setPrefHeight(960);
-        root.setBottom(this.tableView);
+        tableView.setPrefHeight(960);
+        root.setBottom(tableView);
 
         Scene scene = new Scene(root, 800, 600);
         stage.setScene(scene);
         stage.show();
-    }
-
-    /*
-    private void filterTable() {
-        if (this.tableData == null) return;
-
-        String filter1 = this.firstFilteringField.getText().toLowerCase();
-        String column1 = this.firstFilteringColumn.getValue();
-
-        String filter2 = this.secondFilteringField.getText().toLowerCase();
-        String column2 = this.secondFilteringColumn.getValue();
-
-        List<Map<String, String>> filteredData = this.tableData.stream().filter(row -> {
-            boolean matches = true;
-            if (column1 != null && !filter1.isEmpty()) {
-                matches &= row.getOrDefault(column1, "").toLowerCase().contains(filter1);
-            }
-            if (column2 != null && !filter2.isEmpty()) {
-                matches &= row.getOrDefault(column2, "").toLowerCase().contains(filter2);
-            }
-            return matches;
-        }).collect(Collectors.toList());
-
-        this.tableView.setItems(FXCollections.observableArrayList(filteredData));
-    }
-
-    private void exportFilteredData(Stage stage) {
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle(LANG.get(Language.Key.SaveFile));
-        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter(LANG.get(Language.Key.ExcelFiles), "*.xlsx"));
-        File file = fileChooser.showSaveDialog(stage);
-
-        if (file != null) {
-            try (Workbook workbook = new XSSFWorkbook()) {
-                Sheet sheet = workbook.createSheet(LANG.get(Language.Key.FilteredDataSheetName));
-
-                Row headerRow = sheet.createRow(0);
-                List<String> headers = new ArrayList<>(this.tableData.get(0).keySet());
-                for (int i = 0; i < headers.size(); i++) {
-                    headerRow.createCell(i).setCellValue(headers.get(i));
-                }
-
-                List<Map<String, String>> tableData = this.tableView.getItems();
-                for (int i = 0; i < tableData.size(); i++) {
-                    Row row = sheet.createRow(i + 1);
-                    Map<String, String> rowData = tableData.get(i);
-                    for (int j = 0; j < headers.size(); j++) {
-                        row.createCell(j).setCellValue(rowData.get(headers.get(j)));
-                    }
-                }
-
-                try (FileOutputStream fos = new FileOutputStream(file)) {
-                    workbook.write(fos);
-                }
-            } catch (IOException ex) {
-                this.showAlert(LANG.get(Language.Key.ExportError));
-            }
-        }
-    }
-    */
-
-    private void showAlert(String message) {
-        Alert alert = new Alert(Alert.AlertType.ERROR);
-        alert.setTitle(LANG.get(Language.Key.Title));
-        alert.setHeaderText(null);
-        alert.setContentText(message);
-        alert.showAndWait();
-    }
-
-    private File chooseOpenFile() {
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle(LANG.get(Language.Key.SelectFile));
-        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter(LANG.get(Language.Key.ExcelFiles), "*.xls", "*.xlsx", "*.xlsm"));
-        return fileChooser.showOpenDialog(this.stage);
-    }
-
-    private File chooseSaveFile() {
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle(LANG.get(Language.Key.SelectFile));
-        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter(LANG.get(Language.Key.ExcelFiles), "*.xlsx"));
-        return fileChooser.showSaveDialog(this.stage);
-    }
-
-    private Sheet chooseSheet(Workbook workbook) {
-        List<String> sheetNames = new ArrayList<>();
-        workbook.iterator().forEachRemaining(sheet -> sheetNames.add(sheet.getSheetName()));
-
-        ChoiceDialog<String> sheetDialog = new ChoiceDialog<>(sheetNames.get(0), sheetNames);
-        sheetDialog.setTitle(LANG.get(Language.Key.Title));
-        sheetDialog.setHeaderText(null);
-        sheetDialog.setContentText(LANG.get(Language.Key.SelectSheet));
-
-        Optional<String> sheetNameOptional = sheetDialog.showAndWait();
-        return sheetNameOptional.map(workbook::getSheet).orElse(null);
-    }
-
-    private void openFile() {
-        File file = this.chooseOpenFile();
-        try (Workbook workbook = WorkbookFactory.create(file)) {
-            Sheet sheet = this.chooseSheet(workbook);
-            this.tableView.getItems().clear();
-            this.tableView.getColumns().clear();
-            List<List<String>> excelData = this.controller.loadSheet(sheet);
-            ObservableList<ObservableList<String>> data = FXCollections.observableArrayList();
-            excelData.forEach(excelDatum -> data.add(FXCollections.observableArrayList(excelDatum)));
-            this.tableView.setItems(data.stream().skip(1).collect(Collectors.toCollection(FXCollections::observableArrayList)));
-            for (int i = 0; i < excelData.get(0).size(); i++) {
-                int currentColumn = i;
-                TableColumn<ObservableList<String>, String> column = new TableColumn<>(data.get(0).get(i));
-                column.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue().get(currentColumn)));
-                this.tableView.getColumns().add(column);
-            }
-        } catch (Exception ignore) {
-        }
     }
 }
